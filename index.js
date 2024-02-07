36917c418fe3af2169709e59c8ec65032edb6d1b
sakhawat71 <sakhawat.showrav@gmail.com>
2024-02-08 01:53:04 +0600

    cors origin costudy netlify

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
     origin: [
         'https://online-group-study-71.web.app',
-        'http://localhost:5173'
+        'http://localhost:5173',
+        'https://costudy.netlify.app'
     ],
     credentials: true,
 }))
